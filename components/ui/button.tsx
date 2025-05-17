983296ac9ad49b5bb78466c67e7cd559f4d6343e
Denis Vinokurov <dreamofdioma@gmail.com>
2025-05-17 22:42:34 +0300

    next-steam-portfolio-button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium cursor-pointer transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] active:translate-y-[1px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
 	{
 		variants: {
 			variant: {
@@ -35,8 +35,12 @@ const buttonVariants = cva(
 	}
 )
 
-function Button({ className, variant, size, asChild = false, ...props }: React.ComponentProps<"button"> &
-	VariantProps<typeof buttonVariants> & { asChild?: boolean }) {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+	VariantProps<typeof buttonVariants> {
+	asChild?: boolean;
+}
+
+const Button = ({ className, variant, size, asChild = false, ...props }: ButtonProps) => {
 
 	const Comp = asChild ? Slot : "button"
 
