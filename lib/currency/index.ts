93b5858d7b1d806832004b9cefd7e838ddb7a570
Denis Vinokurov <dreamofdioma@gmail.com>
2025-09-05 16:04:27 +0300

    next-steam-portfolio-refactoring

--- lib/currency/index.ts
+++ lib/currency/index.ts
@@ -1,3 +1,4 @@
 export { getExchangeRate } from './get-exchange-rate';
 export { getCurrencyLocale } from './get-currency-locale';
+export { getCurrencySymbol } from './get-currency-symbol';
 export { formatCurrency } from './format-currency';
