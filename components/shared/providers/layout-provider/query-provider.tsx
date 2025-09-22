3a09dc75b0a92cc0e27bcf0d93850bcd6f2b778a
Alexey Buinitsky <alexey-buinitsky@mail.ru>
2025-09-22 20:51:55 +0300

    next-steam-portfolio-refactoring

--- components/shared/providers/layout-provider/query-provider.tsx
+++ components/shared/providers/layout-provider/query-provider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from '@/services/query-client';
 import { ReactNode } from 'react';
 
@@ -9,9 +9,9 @@ export function QueryProvider({ children }: { children: ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-			{process.env.NODE_ENV !== 'production' && (
+			{/* {process.env.NODE_ENV !== 'production' && (
         <ReactQueryDevtools initialIsOpen={false} />
-      )}
+      )} */}
     </QueryClientProvider>
   )
 }
