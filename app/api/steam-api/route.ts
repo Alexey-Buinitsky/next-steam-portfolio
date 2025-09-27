265b2f6cbe3252c1274b1999964b8cc4c061d12b
Alexey Buinitsky <alexey-buinitsky@mail.ru>
2025-09-27 17:34:20 +0300

    next-steam-portfolio-refactoring

--- app/api/steam-api/route.ts
+++ app/api/steam-api/route.ts
@@ -18,7 +18,7 @@ export async function GET(req: NextRequest) {
 		const response = await axios.get('https://steamcommunity.com/market/search/render/',
 			{
 				headers: { 
-					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36', 
+					'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36', 
 					'Accept': 'application/json',
                 	'Accept-Language': 'en-US,en;q=0.9',
 				},
